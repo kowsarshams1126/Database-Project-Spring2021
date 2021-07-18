8bda9b84584a07660ba37fcd57e5fa7f93a8ca77
zeinab kamkar <63596367+zeinabkamkar98@users.noreply.github.com>
2021-07-18 17:46:59 +0430

    my_network_update

--- my_network.py
+++ my_network.py
@@ -2,46 +2,65 @@ import tkinter as tk
 from functools import partial
 from network_query import invitation_data,connection_data,people_know_data,insert_invitation,accept_invitation,reject_invitation
 
+def view_a_user(user_id1,user_id2):
+    pass
+
+#####################################################################################################
 def invitation_page(user_id):
-    
+    #get data 
     inv_data=invitation_data(user_id)
+    #tk view
     window = tk.Tk()
     window.title("INVITATION")
+    counter=0
+    #item[0]=user_id, item[1]=username
     for item in inv_data:
+        counter=counter+1
+        tk.Label(window, text=str(counter)+"- "+item[1]).pack()
         tk.Label(window, text=item[1]).pack()
+        tk.Button(window, text="view",command=lambda: view_a_user(user_id, item[0])).pack()
         tk.Button(window, text="accept",command=partial(accept_invitation,user_id, item[0])).pack()
         tk.Button(window, text="reject", command=partial(reject_invitation,user_id, item[0])).pack()
         tk.Label(window, text="------------------").pack()
-
     window.geometry('400x400')
     window.mainloop()
 
+#####################################################################################################
 def people_know_page(user_id):
-    
+    #get data
     know_data=people_know_data(user_id)
+    #tk view
     window = tk.Tk()
     window.title("PEOPLE YOU MAY KNOW")
+    counter=0
+    #item[0]=user_id, item[1]=username
     for item in know_data:
-        tk.Label(window, text=item[1]).pack()
-        # tk.Button(window, text="view",command=lambda: view_a_user(user_id, item[0])).pack()
+        counter=counter+1
+        tk.Label(window, text=str(counter)+"- "+item[1]).pack()
+        tk.Button(window, text="view",command=lambda: view_a_user(user_id, item[0])).pack()
         tk.Button(window, text="invitation", command=partial(insert_invitation,user_id, item[0])).pack()
         tk.Label(window, text="------------------").pack()
-
     window.geometry('400x400')
     window.mainloop()
     
+#####################################################################################################
 def connection_page(user_id):
+    #get data 
     conn_data=connection_data(user_id)
+    #tk-view
     window = tk.Tk()
     window.title("CONNECTIOIN")
-    for item in conn_data:
-        tk.Label(window, text=item[1]).pack()
-        # tk.Button(window, text="view",command=partial(view,user_id,item[0])).pack()
+    counter=0
+    #item[0]=user_id, item[1]=username
+    for item in conn_data: 
+        counter=counter+1
+        tk.Label(window, text=str(counter)+"- "+item[1]).pack()
+        tk.Button(window, text="view",command=lambda: view_a_user(user_id, item[0])).pack()
         tk.Label(window, text="------------------").pack()
-
     window.geometry('400x400')
     window.mainloop()
     
+#####################################################################################################
 def my_network_page(user_id):
 
     window = tk.Tk()
@@ -51,8 +70,9 @@ def my_network_page(user_id):
     tk.Button(window, text="people you may khow", command=lambda: people_know_page(user_id)).pack()
     window.geometry('400x400')
     window.mainloop()
-    
+
+#####################################################################################################    
 def main():
-    my_network_page(3);
+    my_network_page(1);
 
 main()
--- network_query.py
+++ network_query.py
@@ -1,44 +1,7 @@
 import sqlite3
 
-def invitation_data(user_id):
-    con = sqlite3.connect('linkedin_db.db')
-    cur = con.cursor()
-    
-    data=cur.execute('SELECT user_id,username FROM user WHERE user_id IN (SELECT user_idT FROM invitation WHERE user_idR=?)',(user_id,)).fetchall()
 
-    con.commit()
-    con.close()
-    return data
-
-def insert_invitation(user_idT,user_idR):
-    con = sqlite3.connect('linkedin_db.db')
-    cur = con.cursor()
-    
-    cur.execute("INSERT INTO invitation (user_idT,user_idR) VALUES(?,?)",(user_idT,user_idR,))
-
-    con.commit()
-    con.close()    
-
-def accept_invitation(user_id1,user_id2):
-    con = sqlite3.connect('linkedin_db.db')
-    cur = con.cursor()
-    
-    cur.execute("INSERT INTO connection (user_id1,user_id2) VALUES(?,?)",(user_id1,user_id2,))
-    cur.execute("DELETE FROM invitation WHERE  user_idT=? AND user_idR=?",(user_id2,user_id1,))
-
-    con.commit()
-    con.close()
-
-def reject_invitation(user_id1,user_id2):
-    con = sqlite3.connect('linkedin_db.db')
-    cur = con.cursor()
-    
-    cur.execute("DELETE FROM invitation WHERE user_idT=? AND user_idR=?",(user_id2,user_id1,))
-
-    con.commit()
-    con.close()
-
-
+###########################################################################
 def connection_data(user_id):
     con = sqlite3.connect('linkedin_db.db')
     cur = con.cursor()
@@ -55,6 +18,8 @@ def connection_data(user_id):
     con.commit()
     con.close()
     return data    
+
+###########################################################################
 
 def people_know_data(user_id):
     con = sqlite3.connect('linkedin_db.db')
@@ -119,3 +84,58 @@ def people_know_data(user_id):
     con.commit()
     con.close()
     return data   
+
+
+def insert_invitation(user_idT,user_idR):
+    con = sqlite3.connect('linkedin_db.db')
+    cur = con.cursor()
+    
+    number=cur.execute('SELECT count(user_idT) FROM invitation WHERE user_idR=? AND user_idT=?',(user_idR,user_idT)).fetchall()
+    
+    if number[0][0]==0:
+        cur.execute("INSERT INTO invitation (user_idT,user_idR) VALUES(?,?)",(user_idT,user_idR,))
+    else:
+        print("you already send invitation to this user")
+        
+    con.commit()
+    con.close()    
+
+#################################################################################################################
+def invitation_data(user_id):
+    con = sqlite3.connect('linkedin_db.db')
+    cur = con.cursor()
+    
+    data=cur.execute('SELECT user_id,username FROM user WHERE user_id IN (SELECT user_idT FROM invitation WHERE user_idR=?)'
+                     ,(user_id,)).fetchall()
+
+    con.commit()
+    con.close()
+    return data
+
+def accept_invitation(user_idR,user_idT):
+    con = sqlite3.connect('linkedin_db.db')
+    cur = con.cursor()
+    
+    number=cur.execute("SELECT count(user_id1) FROM connection WHERE (user_id1=? AND user_id2=?) OR (user_id1=? AND user_id2=?)"
+                ,(user_idR,user_idT,user_idT,user_idR,)).fetchall()
+    
+    if number[0][0]==0:
+        cur.execute("INSERT INTO connection (user_id1,user_id2) VALUES(?,?)",(user_idR,user_idT,))
+        cur.execute("DELETE FROM invitation WHERE  user_idT=? AND user_idR=?",(user_idT,user_idR,))
+
+    else:
+        print("you already accept this invitation")
+        
+    con.commit()
+    con.close()
+
+def reject_invitation(user_id1,user_id2):
+    con = sqlite3.connect('linkedin_db.db')
+    cur = con.cursor()
+    
+    cur.execute("DELETE FROM invitation WHERE user_idT=? AND user_idR=?",(user_id2,user_id1,))
+
+    con.commit()
+    con.close()
+
+#################################################################################################################
