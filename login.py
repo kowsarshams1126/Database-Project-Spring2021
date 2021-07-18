3cf9864edeeed1aed13dd83bc943d05a7e34dfec
kowsar <47795691+kowsarshams1126@users.noreply.github.com>
2021-07-19 00:54:56 +0430

    Update login.py

--- login.py
+++ login.py
@@ -5,36 +5,51 @@ import profile
 
 con = sqlite3.connect('linkedin_db.db')
 cur = con.cursor()
-def checkInDB2(username, password):
-    res=cur.execute(f'select user_id from user where name="{username}" and password="{password}"')
-    return False if len(res)==0 else True
+
+
+def checkInDB2(username1, password):
+    res = cur.execute(f'select user_id from user where username="{username1}" and password="{password}"').fetchall()
+    print(res)
+    return False if len(res) == 0 else True
+
 
 def Authentication(loginpage, username1, password):
     global UID
     loginpage.destroy()
-    if not checkInDB2(username1, password):
-        UID=username1.get()
+    if checkInDB2(username1, password):
+        UID = username1
+        profile.profile_mainPage(UID)
     else:
         login(True)
 
+
 def checkInDB(username, email):
     res = cur.execute(f'select user_id from user where name="{username}" or email="{email}"').fetchall()
-    return True if len(res) == 0 else False
-def sendData2DB_NEWACCOUNT(newAccount, username, password, email):
+    print(res)
+    print("LLLLLLLLLLLLLLLLLLLLLLLL")
+    return False if len(res) == 0 else True
+
+
+def sendData2DB_NEWACCOUNT(newAccount, username, password, email,loginPage):
     global UID
     newAccount.destroy()
-    if ( checkInDB(username, password)):
+    print(checkInDB(username, password))
+    if (not checkInDB(username, email)):
         print("WHY")
-        print(cur.execute(f'insert into user(name,password,email) values ("{username}","{password}","{email}") '))
+        print(cur.execute(
+            f'insert into user(name,password,email,username) values ("{username}","{password}","{email}","{username}") '))
         con.commit()
         UID = username
+        loginPage.destroy()
         profile.profile_mainPage(UID)
         return True
     else:
-        Create(True)
+        Create(True,loginPage)
         return False
 
-def Create(flag):
+
+def Create(flag,loginPage):
+    # loginPage.destroy()
     newAccount = Tk()
     usernameLabel = Label(newAccount, text='username')
     passwordLabel = Label(newAccount, text='password')
@@ -53,34 +68,34 @@ def Create(flag):
     emailInput.grid(row=3, column=2)
     submit = Button(newAccount, text='Submit',
                     command=lambda: sendData2DB_NEWACCOUNT(newAccount, usernameInput.get(), passwordInput.get(),
-                                                           emailInput.get()))
+                                                           emailInput.get(),loginPage))
     submit.grid(row=4, column=1)
     newAccount.mainloop()
 
 
-
 def login(flag):
     global username
-    loginPage=Tk("500x5000")
-    usernameLabel=Label(loginPage, text='username')
-    passwordLabel=Label(loginPage, text='password')
-    username=StringVar()
-    password=StringVar()
-    usernameInput=Entry(loginPage, textvariable=username)
-    passwordInput=Entry(loginPage,show="*",textvariable=password)
-    lab=Label(loginPage,text= "Wrong username or password! Please Retry." if flag else "                                               ")
-    lab.grid(row=0,column=2)
+    loginPage = Tk("500x5000")
+    usernameLabel = Label(loginPage, text='username')
+    passwordLabel = Label(loginPage, text='password')
+    username = StringVar()
+    password = StringVar()
+    usernameInput = Entry(loginPage, textvariable=username)
+    passwordInput = Entry(loginPage, show="*", textvariable=password)
+    lab = Label(loginPage,
+                text="Wrong username or password! Please Retry." if flag else "                                               ")
+    lab.grid(row=0, column=2)
     usernameLabel.grid(row=1, column=1)
     passwordLabel.grid(row=2, column=1)
-    usernameInput.grid(row=1,column=2)
-    passwordInput.grid(row=2,column=2)
-    loginButton=Button(loginPage,text='Sign in',command=lambda :Authentication(loginPage,username,password))
-    signUpButton=Button(loginPage,text='Sign up',command=lambda :Create(False))
-    loginButton.grid(row=3,column=1)
-    signUpButton.grid(row=3,column=2)
+    usernameInput.grid(row=1, column=2)
+    passwordInput.grid(row=2, column=2)
+    loginButton = Button(loginPage, text='Sign in',
+                         command=lambda: Authentication(loginPage, username.get(), password.get()))
+    signUpButton = Button(loginPage, text='Sign up', command=lambda: Create(False,loginPage))
+    loginButton.grid(row=3, column=1)
+    signUpButton.grid(row=3, column=2)
     # username=username.get()
     # password=password.get()
     print(username)
     print(password)
     loginPage.mainloop()
-
