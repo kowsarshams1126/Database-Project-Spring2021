6babb89d6bb6d634b07d734f3e3562e409185412
kowsar <47795691+kowsarshams1126@users.noreply.github.com>
2021-07-25 20:33:58 +0430

    Update notif_query.py

--- notif_query.py
+++ notif_query.py
@@ -15,46 +15,50 @@ def addNotif_birthday(user_id, param):
     con.close()
 
 def addNotif_viewProfile(user_id, param):
-    con = sqlite3.connect('linkedin_db.db')
-    cur = con.cursor()
-    res = cur.execute(
-        f'select notification_id from notification where type=2 and user_idT="{user_id}" and user_idR="{param}"').fetchall()
-    if len(res) == 0:
-        cur.execute(
-            f'insert into notification(read,type,user_idT,user_idR,date) values ("{0}","{2}","{user_id}","{param}","{datetime.datetime.now()}")')
-    
-    else:
-        cur.execute(
-            f'update notification set read="{0}",date="{datetime.datetime.now()}" where notification_id="{res[0][0]}"')
-        con.commit()
-    con.close()
+    if user_id!=param:
+        con = sqlite3.connect('linkedin_db.db')
+        cur = con.cursor()
+        res = cur.execute(
+            f'select notification_id from notification where type=2 and user_idT="{user_id}" and user_idR="{param}"').fetchall()
+        if len(res) == 0:
+            cur.execute(
+                f'insert into notification(read,type,user_idT,user_idR,date) values ("{0}","{2}","{user_id}","{param}","{datetime.datetime.now()}")')
+
+        else:
+            cur.execute(
+                f'update notification set read="{0}",date="{datetime.datetime.now()}" where notification_id="{res[0][0]}"')
+            con.commit()
+        con.close()
 
 
 def addNotif_likePost(user_id, param):
-    con = sqlite3.connect('linkedin_db.db')
-    cur = con.cursor()
-    cur.execute(
-        f'insert into notification(read,type,user_idT,user_idR,date) values ("{0}","{3}","{user_id}","{param}","{datetime.datetime.now()}")')
-    con.commit()
-    con.close()
+    if user_id!=param:
+        con = sqlite3.connect('linkedin_db.db')
+        cur = con.cursor()
+        cur.execute(
+            f'insert into notification(read,type,user_idT,user_idR,date) values ("{0}","{3}","{user_id}","{param}","{datetime.datetime.now()}")')
+        con.commit()
+        con.close()
 
 
 def addNotif_comment(user_id, param):
-    con = sqlite3.connect('linkedin_db.db')
-    cur = con.cursor()
-    cur.execute(
-        f'insert into notification(read,type,user_idT,user_idR,date) values ("{0}","{4}","{user_id}","{param}","{datetime.datetime.now()}")')
-    con.commit()
-    con.close()
+    if user_id!=param:
+        con = sqlite3.connect('linkedin_db.db')
+        cur = con.cursor()
+        cur.execute(
+            f'insert into notification(read,type,user_idT,user_idR,date) values ("{0}","{4}","{user_id}","{param}","{datetime.datetime.now()}")')
+        con.commit()
+        con.close()
 
 
 def addNotif_likeOrReplyComment(user_id, param):
-    con = sqlite3.connect('linkedin_db.db')
-    cur = con.cursor()
-    cur.execute(
-        f'insert into notification(read,type,user_idT,user_idR,date) values ("{0}","{5}","{user_id}","{param}","{datetime.datetime.now()}")')
-    con.commit()
-    con.close()
+    if user_id!=param:
+        con = sqlite3.connect('linkedin_db.db')
+        cur = con.cursor()
+        cur.execute(
+            f'insert into notification(read,type,user_idT,user_idR,date) values ("{0}","{5}","{user_id}","{param}","{datetime.datetime.now()}")')
+        con.commit()
+        con.close()
 
 def addNotif_endorse(user_id, param):
     con = sqlite3.connect('linkedin_db.db')
